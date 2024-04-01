11c522504d4ea259f75231bb8314e49cc5132d58
Bokang412 <151556695+Bokang412@users.noreply.github.com>
2024-04-01 13:43:49 +0200

    Add files via upload

---
+++ Chapter 9/Chapter 9.js
@@ -0,0 +1,16 @@
+// Practical exercise 1
+console.dir(window);              // Opens up a window displaying info about a website
+console.dir(window.document);     // Brings the info about the document
+console.dir(window.innerHeight);  // Finds inner height of window in pixels
+console.dir(window.innerWidth);   // Finds inner width of window in pixels
+console.dir(window.outerHeight);  // Finds outer height of window in pixels
+console.dir(window.outerWidth);   // Finds outer width of window in pixels
+
+// Practical exercise 2
+console.dir(window.location);             // Finds the location of the current file
+console.dir(window.location.protocol);    // Finds the protocol in the location of the current file
+console.dir(window.location.href);        // Finds the href in the location of the current file
+
+// Practical exercise 3
+// Check on HTML file
+
